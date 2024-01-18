fbbaa65b771dd1191e5bedd0554daa6babbb9d43
Johannes Ström <johannes.strom@svenskaspel.se>
2024-01-18 08:57:48 +0100

    introduced inheritance to components

--- scripts/components/button.py
+++ scripts/components/button.py
@@ -1,7 +1,7 @@
 import pygame
 import text as txt
 
-class Buttion: # Renders a button with text
+class Button: # Renders a button with text
   def __init__(self, position, size, color):
     self.color = color
     self.pos = position
@@ -12,30 +12,22 @@ class Buttion: # Renders a button with text
   def render(self, surface):
     pygame.draw.rect(surface, self.button_color, self.button_rect)
 
-class TextButtion: # Renders a button with text
+class TextButton(Button): # Renders a button with text
   def __init__(self, position, text, size, color, text_color):
-    self.color = color
-    self.pos = position
-    self.size = size
-    self.button_rect = pygame.Rect(position[0], position[1], size[0], size[1])  
-    self.button_color  = color
+    super().__init__(position, size, color)
     self.text_obj = txt.Text(position, text, text_color, True)
 
   def render(self, surface):
-    pygame.draw.rect(surface, self.button_color, self.button_rect)
+    super().render(surface)
     self.text_obj.render(surface)
 
 
-class IconButtion: # Renders a button with an icon
+class IconButton(Button): # Renders a button with an icon
   def __init__(self, iconPath, position, size, color):
+    super().__init__(position, size, color)
     icon = pygame.image.load(iconPath)
     self.icon = pygame.transform.scale(icon, (50, 50))
-    self.color = color
-    self.pos = position
-    self.size = size
-    self.button_rect = pygame.Rect(position[0], position[1], size[0], size[1])  
-    self.button_color  = color  
 
   def render(self, surface):
-    pygame.draw.rect(surface, self.button_color, self.button_rect)
+    super().render(surface)
     surface.blit(self.icon, (self.button_rect.x, self.button_rect.y,))

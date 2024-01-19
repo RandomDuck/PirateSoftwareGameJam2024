a28527df6ee7c956fbf70be65bc228a62c73b75c
Johannes Ström <johannes.strom@svenskaspel.se>
2024-01-19 08:56:40 +0100

    removed test code

--- init.py
+++ init.py
@@ -6,7 +6,6 @@ pygame.init()
 screen = pygame.display.set_mode((720, 1080))
 clock = pygame.time.Clock()
 running = True
-x, y = 50, 50
 
 button = btn.TextButton((20,20), "Hello world", (screen.get_rect().width-40,50), ((255,255,255),(255,127,127)), (255,0,0), (True, True))
 
@@ -20,16 +19,8 @@ while running:
         if event.type == pygame.MOUSEBUTTONUP:
             triggerMouseEvents = True
     
-    # Get the mouse position
-    mouseX, mouseY = pygame.mouse.get_pos()
-
-    # Check if the left mouse button is pressed
-    if pygame.mouse.get_pressed()[0]:
-        x, y = mouseX, mouseY
-
     screen.fill((0, 0, 0))  # Fill the window with black
     button.render(screen, triggerMouseEvents)
-    pygame.draw.rect(screen, (255, 0, 0), (x, y, 5, 5))  # Draw a red rectangle
 
 
     # flip() the display to put your work on screen

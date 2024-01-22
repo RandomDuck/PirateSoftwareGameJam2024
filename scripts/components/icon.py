c6f303cfb6d5136c9e1dff327f01c9acab95fc04
Johannes Ström <johannes.strom@svenskaspel.se>
2024-01-22 10:06:33 +0100

    made icon class

--- scripts/components/icon.py
+++ scripts/components/icon.py
@@ -1,1 +1,16 @@
-# TODO: make class for importing and displaying icons
+import pygame
+
+class Icon:
+  def __init__(self, image_path, position, size):
+    self.original_image = pygame.image.load(image_path)
+    self.size = size
+    self.position = position
+    self.image = pygame.transform.scale(self.original_image, size)
+
+  def render(self, surface):
+    surface.blit(self.image, self.position)
+
+  def update(self, position, size):
+    self.size = size
+    self.position = position
+    self.image = pygame.transform.scale(self.original_image, size)

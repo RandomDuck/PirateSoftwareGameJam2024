99e12aa20b0620d44a66957d60eeed955dcdcff9
Johannes Ström <johannes.strom@svenskaspel.se>
2024-01-26 07:55:33 +0100

    new icons

--- resources/datasets/storeItems.py
+++ resources/datasets/storeItems.py
@@ -1,8 +1,8 @@
 storeItems = [
   {
     "id": 0,
-    "title": "Default",
-    "description": "A default store item",
+    "title": "Get MountainDew",
+    "description": "Gives you more energy",
     "cost": 10,
     "follows": 0,
     "fps": 0.3,
@@ -13,8 +13,26 @@ storeItems = [
     "title": "Botfarm",
     "description": "Hire bots to like your posts",
     "cost": 100,
-    "follows": -2,
-    "fps": 0.3,
+    "follows": 0,
+    "fps": 1,
+    "iconPath": "resources/storeIcons/default.png"
+  },
+  {
+    "id": 2,
+    "title": "Food fight",
+    "description": "Throw food in peoples face",
+    "cost": 400,
+    "follows": 0,
+    "fps": 5,
+    "iconPath": "resources/storeIcons/default.png"
+  },
+  {
+    "id": 3,
+    "title": "Hack Ludwig",
+    "description": "Generate ads on popular streamer Ludwig's profile",
+    "cost": 1200,
+    "follows": 0,
+    "fps": 30,
     "iconPath": "resources/storeIcons/default.png"
   }
 ]
--- scripts/modules/profile.py
+++ scripts/modules/profile.py
@@ -1,5 +1,6 @@
 from ..components.page import Page
 from ..components.text import TextBox, Text
+from ..components.icon import IconBox
 
 class ProfilePage(Page):
   def __init__(self, screen, area, gameCon):
@@ -8,15 +9,17 @@ class ProfilePage(Page):
     
   def setup(self):
     self.banner = TextBox(self.pos,(self.size[0],30),"Profile",(255,255,255),(0,200,0),(True,True))
-    self.user = TextBox((self.pos[0]+10, self.pos[1]+40), (self.size[0]-20,50),"Tharr plunderer of scripts",(255,255,255),(0,100,0),(True,True))
-    self.cash = Text((self.pos[0]+10, self.pos[1]+100), (self.size[0]-20,30),f"Cash: {self.gameCon.getCash()}",(0,100,0),(True,True))
-    self.follows = Text((self.pos[0]+10, self.pos[1]+130), (self.size[0]-20,30),f"Follows: {self.gameCon.getFollowers()}",(0,100,0),(True,True))
-    self.fps = Text((self.pos[0]+10, self.pos[1]+160), (self.size[0]-20,30),f"Followers per second: {self.gameCon.getfollowersPerSecond()}",(0,100,0),(True,True))
+    self.user = TextBox((self.pos[0]+10, self.pos[1]+45), (self.size[0]-20,40),"Tharr plunderer of scripts",(255,255,255),(0,100,0),(True,True))
+    self.cash = Text((self.pos[0]+120, self.pos[1]+100), (self.size[0]-130,30),f"Cash: {self.gameCon.getCash()}",(0,100,0),(True,False))
+    self.follows = Text((self.pos[0]+120, self.pos[1]+130), (self.size[0]-130,30),f"Follows: {self.gameCon.getFollowers()}",(0,100,0),(True,False))
+    self.fps = Text((self.pos[0]+120, self.pos[1]+160), (self.size[0]-130,30),f"Followers per second: {self.gameCon.getfollowersPerSecond()}",(0,100,0),(True,False))
+    self.icon = IconBox("resources/profile.png",(self.pos[0]+10, self.pos[1]+95), (100,100), (0,125,125), 5)
     self.elements.append(self.banner)
     self.elements.append(self.user)
     self.elements.append(self.follows)
     self.elements.append(self.cash)
     self.elements.append(self.fps)
+    self.elements.append(self.icon)
 
   def render(self, events):
     super().render(events)
@@ -27,8 +30,9 @@ class ProfilePage(Page):
   def update(self, area):
     super().update(area)
     self.banner.update(self.pos, (self.size[0],30))
-    self.user.update((self.pos[0]+10, self.pos[1]+40), (self.size[0]-20,50))
-    self.cash.update((self.pos[0]+10, self.pos[1]+100), (self.size[0]-20,30))
-    self.follows.update((self.pos[0]+10, self.pos[1]+130), (self.size[0]-20,30))
-    self.fps.update((self.pos[0]+10, self.pos[1]+160), (self.size[0]-20,30))
+    self.user.update((self.pos[0]+10, self.pos[1]+45), (self.size[0]-20,40))
+    self.cash.update((self.pos[0]+120, self.pos[1]+100), (self.size[0]-130,30))
+    self.follows.update((self.pos[0]+120, self.pos[1]+130), (self.size[0]-130,30))
+    self.fps.update((self.pos[0]+120, self.pos[1]+160), (self.size[0]-130,30))
+    self.icon.update((self.pos[0]+10, self.pos[1]+95), (100,100))
     #sizeCalc = self.size[1]-190

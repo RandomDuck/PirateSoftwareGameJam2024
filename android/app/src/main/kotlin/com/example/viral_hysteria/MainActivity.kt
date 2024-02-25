78b0569659fdb952170a15f78068aba3ef28901e
Johannes Ström <johannes.strom@svenskaspel.se>
2024-02-25 13:25:32 +0100

    setup flutter

---
+++ android/app/src/main/kotlin/com/example/viral_hysteria/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.viral_hysteria
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/resources/datasets/storeItems.py
@@ -0,0 +1,40 @@
+from scripts.components.utils import resource_path
+storeItems = [
+  {
+    "id": 0,
+    "title": "Get MountainDew",
+    "description": "Gives you more energy",
+    "cost": 10,
+    "follows": 0,
+    "fps": 0.3,
+    "iconPath": resource_path("resources/storeIcons/default.png")
+  },
+  {
+    "id": 1,
+    "title": "Botfarm",
+    "description": "Hire bots to like your posts",
+    "cost": 100,
+    "follows": 0,
+    "fps": 1,
+    "iconPath": resource_path("resources/storeIcons/default.png")
+  },
+  {
+    "id": 2,
+    "title": "Food fight",
+    "description": "Throw food in peoples face",
+    "cost": 400,
+    "follows": 0,
+    "fps": 5,
+    "iconPath": resource_path("resources/storeIcons/default.png")
+  },
+  {
+    "id": 3,
+    "title": "Hack Ludwig",
+    "description": "Generate ads on popular streamer Ludwig's profile",
+    "cost": 1200,
+    "follows": 0,
+    "fps": 30,
+    "iconPath": resource_path("resources/storeIcons/default.png")
+  }
+]
+
